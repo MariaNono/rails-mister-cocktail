dac23ecee464f69f9ae620049ec2e7438ccae18e
Maria Nogueira <leo.ds.nogueira@gmail.com>
2019-11-15 15:50:00 +0100

    finallyyy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+
+# INGREDIENTS
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url_json = open(url).read
+data = JSON.parse(url_json)
+
+data['drinks'].each do |ing|
+  Ingredient.create(name: ing["strIngredient1"])
+end
+
+
+# COCKTAIL
+
+mojito = Cocktail.create(name: 'Mojito')
+cuba_libre = Cocktail.create(name: 'Cuba Libre')
+bloody_mary = Cocktail.create(name: 'Bloody Mary')
+pina_colada = Cocktail.create(name: 'Piña colada')
+moscow_mule = Cocktail.create(name: 'Moscow Mule')
+margarita = Cocktail.create(name: 'Margarita')
+caipirinha = Cocktail.create(name: 'Caipirinha')
+
+
+# DOSES
